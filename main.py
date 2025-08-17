3aaf934a69aa0583e30bd3f0879553ccdbec27a3
Ranajit Mitra <ranajitmitra17@gmail.com>
2025-08-17 22:16:23 +0530

    modified

--- main.py
+++ main.py
@@ -6,40 +6,38 @@ from rctc import ctext
 init(autoreset=True)
 
 while True:
-    print('\n')
+    print("\n")
     print(f"{ctext("⫸ ^_^ WELCOME TO THE GAME ^_^", repeat=3, delay=0.5)}")
     time.sleep(2)
     print(ctext("⫸ How many rounds you want to play: ", repeat=2, delay=0.5))
-    round = input('⫸')
+    round = input("⫸")
     try:
         int_round = int(round)
         break
     except:
         print("❕enter in integer")
 
-game = [
-    "rock",
-    "paper",
-    "sissor"
-    ]
+game = ["rock", "paper", "sissor"]
 random_item = random.choice(game)
 score = 0
 for i in range(int_round):
-    print(Fore.GREEN + Style.BRIGHT + f"Round: {i+1}" + Style.RESET_ALL )
-    user = input("Choose (Rock, Paper, Sissor) : ").strip().lower()
-    if user not in ["rock","paper","sissor"]:
+    print(Fore.GREEN + Style.BRIGHT + f"ROUND: {i+1}" + Style.RESET_ALL)
+    user = input("🤔 Choose (Rock, Paper, Sissor) : ").strip().lower()
+    if user not in ["rock", "paper", "sissor"]:
         print("choose in (Rock, Paper, Sissor)")
     else:
-        print("bot choosing...")
+        print("🤖 bot choosing...")
         time.sleep(1)
         if user == random_item:
-            print("You win")
+            print("🏆 You win")
             score += 1
-            print(score)
+        else:
+            print("💔 You loose")
+        print('\n')
 
-        else: 
-            print("You loose")
+if score > 0:
+    print(f"😍 You scored {score}")
+else:
+    print(f"😞 You scored {score}")
 
-   
-print(f"You scored {score}")
-
+    
--- rctc.py
+++ rctc.py
@@ -2,25 +2,16 @@ import random
 import time
 from colorama import init, Fore
 
+
 def ctext(text: str, repeat: int, delay: int) -> None:
-    colors = [
-            Fore.RED, 
-            Fore.GREEN, 
-            Fore.BLUE, 
-            Fore.YELLOW, 
-            Fore.CYAN, 
-            Fore.MAGENTA
-            ]
+    colors = [Fore.RED, Fore.GREEN, Fore.BLUE, Fore.YELLOW, Fore.CYAN, Fore.MAGENTA]
 
     for i in range(repeat):
-        print(
-            colors[i % len(colors)] + text, 
-            end='\r', 
-            flush=True
-            )
+        print(colors[i % len(colors)] + text, end="\r", flush=True)
         time.sleep(delay)
     r_color = random.choice(colors)
     return r_color + text
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     ctext()
